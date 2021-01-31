c2fa7d019de7f976533969f868e8c09f13d406c3
Christerle Nounoute <christerlesn@gmail.com>
2021-01-31 22:17:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    new_puppy = Puppy.new(params(params[:name] && params[:breed] &&
-    params[:age])
+    new_puppy = Puppy.new(params(params[:name])
     @puppy = new_puppy
 
     erb :display_puppy
